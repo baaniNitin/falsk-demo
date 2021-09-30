38e874d1530130d43b5594d2ea4554487ceade4d
baaniNitin <43900806+baaniNitin@users.noreply.github.com>
2021-09-30 20:23:02 +0530

    Update build.sh
    updating build script

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker build -t flaskapp /root/flask/
+result=$( sudo docker images -q flaskapp:latest )
+
+if [[ -n "$result" ]]; then
+  echo "Container exists"
+  kubectl delete deployment flask-test-app
+  docker rmi flaskapp
+else
+  echo "No such container"
+  docker build -t flaskapp /root/flask/
+fi
